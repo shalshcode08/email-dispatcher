ec25b987c4ae2f6854f53c73342eb94ee5532993
shalshcode08 <somushrestha448@gmail.com>
2026-01-15 17:36:04 +0530

    update: producer ready

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+type Recipient struct {
+	Name  string
+	Email string
+}
 
+func main() {
+	recipentChannel := make(chan Recipient)
+	loadRecipients("./emails.csv", recipentChannel)
 }
